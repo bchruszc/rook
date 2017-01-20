9c2f4a57a4598c73f62d1aee55d8ee56a72cf52f
bchruszc <bradley.chruszcz@gmail.com>
2017-01-19 23:23:06 -0500

    Changes to support current quirks with player scores

--- rookscore/serializers.py
+++ rookscore/serializers.py
@@ -8,7 +8,8 @@ from rookscore import utils
 import logging
 
 # Get an instance of a logger
-logger = logging.getLogger(__name__)
+logger = logging.getLogger('rook2_beta')
+
 
 # Serializers define the API representation.
 class PlayerSerializer(serializers.ModelSerializer):
@@ -16,30 +17,55 @@ class PlayerSerializer(serializers.ModelSerializer):
         model = Player
         fields = ('id', 'player_id', 'first_name', 'last_name')
 
+    # # We really just need the ID
+    # def validate(self, data):
+    #     logger.debug(data)
+    #     return data['id']
+
 # ViewSets define the view behavior.
 class PlayerViewSet(viewsets.ModelViewSet):
     queryset = Player.objects.all()
     serializer_class = PlayerSerializer
 
+#
+# For whatever reason I made it so that the "Scores" player sends extra data - I assume so that on-demand adding of
+# players was possible
+#
+# Handling that requires a special lookup, since we really only care about the ID.  Maybe in a future version of the
+# app this can be cleaned up, and making this method sopport either type may help the transition...
+#
+class PlayerDetailedLookupField(serializers.RelatedField):
+    # The only time we return this field is in a 201 after a successful create.  Returning None should have no
+    # practical impact.
+    def to_representation(self, value):
+        return None
+
+    def to_internal_value(self, data):
+        logger.debug('Converting player to internal value')
+        logger.debug(data)
+        return Player.objects.get(pk=data['id'])
+
+
 class ScoreSerializer(serializers.ModelSerializer):
-    # player = serializers.PrimaryKeyRelatedField(read_only=True)
-    
+    player = PlayerDetailedLookupField(queryset=Player.objects.all())
+
     class Meta:
         model = PlayerGameSummary
         fields = ('id', 'player', 'rank', 'score', 'made_bid')
-        
+
+
 class ScoreViewSet(viewsets.ModelViewSet):
     queryset = PlayerGameSummary.objects.all()
     serializer_class = ScoreSerializer
+
 
 class BidSerializer(serializers.ModelSerializer):
     class Meta:
         model = Bid
         fields = ('caller', 'partners', 'opponents', 'points_bid', 'points_made')
 
+
 class GameSerializer(serializers.ModelSerializer):
-    #    scores = serializers.HyperlinkedRelatedField(many=True, view_name='score-detail')
-    #scores = serializers.RelatedField(many=True)
     scores = ScoreSerializer(many=True)
     bids = BidSerializer(many=True)
 
@@ -48,31 +74,29 @@ class GameSerializer(serializers.ModelSerializer):
         fields = ('id', 'entered_date', 'played_date', 'scores', 'bids')
 
     def create(self, validated_data):
-        #print '***** SERIALIZING GAME ********'
-        print (validated_data)
+        # print(validated_data)
 
         scores_data = validated_data.pop('scores')
         bids_data = validated_data.pop('bids')
         game = Game.objects.create(**validated_data)
-        print ("Created game: " + str(game))
-        print(str(game.id))
+        logger.debug("Created game: " + str(game))
 
         summaries = []
-        
+
         for bid_data in bids_data:
-            print ("Creating bid: " + str(bid_data))
+            logger.debug("Creating bid: " + str(bid_data))
             # Bid.objects.create(game=game, **bid_data)
             b = Bid()
             b.game = game
-            b.caller=bid_data['caller']
-            b.points_bid=bid_data['points_bid']
-            b.points_made=bid_data['points_made']
+            b.caller = bid_data['caller']
+            b.points_bid = bid_data['points_bid']
+            b.points_made = bid_data['points_made']
             b.save()
-            
+
             # Apparently you need to save the bid before creating the many-to-many fields
             # Makes some sense, but the errors were pretty useless...
-            b.partners=bid_data['partners']
-            b.opponents=bid_data['opponents']
+            b.partners = bid_data['partners']
+            b.opponents = bid_data['opponents']
             b.save()
             # Bid.objects.create(game=game, 
             #     caller=bid_data['caller'], 
@@ -85,25 +109,22 @@ class GameSerializer(serializers.ModelSerializer):
 
 
             pass
-        for score_data in scores_data:  
-            print (str(score_data))
-            player = score_data['player']
-            print ('Player Data: ' +  str(player))
-            # player = Player.objects.get(player_id=player_data['player_id'])
+        for score_data in scores_data:
+            logger.debug(str(score_data))
+
             summary = PlayerGameSummary.objects.create(game=game, **score_data)
             summaries.append(summary)
+
         # Write a utility to generate ranks and apply them, given a list of summaries - link to the HTML entry
         utils.sortAndRankSummaries(summaries)
-        
+
         for s in summaries:
             s.save()
 
         return game
-    
 
-        
+
 # ViewSets define the view behavior.
 class GameViewSet(viewsets.ModelViewSet):
     queryset = Game.objects.all()
     serializer_class = GameSerializer
-    
--- rookscore/views.py
+++ rookscore/views.py
@@ -1,5 +1,3 @@
-from __future__ import print_function
-
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.template import RequestContext, loader
 from django.shortcuts import render
@@ -23,71 +21,77 @@ from rest_framework import permissions
 from datetime import datetime
 from rookscore import utils
 
+import logging
+
+logger = logging.getLogger('rook2_beta')
+
 def index(request):
     season = CacheManager().seasons().get(datetime.today())
     return render_season(request, season)
 
+
 # Render a specific season, or all if season = None
 def render_season(request, season):
     rankings = Player.objects.rankings(season)
-    
+
     # Show most recent first, if a season limit to that season, and just the last 5
     recent_game_list = Game.objects.order_by('-played_date')
     if season:
         recent_game_list = recent_game_list.filter(played_date__lte=season.end_date)
-    
+
     recent_game_list = recent_game_list[:5]
     template = loader.get_template('rookscore/index.html')
     context = RequestContext(request, {
-        'season':season,
+        'season': season,
         'rankings': rankings,
-        'recent_game_list':recent_game_list,
+        'recent_game_list': recent_game_list,
         'settings': settings,
     })
     return HttpResponse(template.render(context))
 
+
 def entry(request):
-    if request.method == 'POST': # If the form has been submitted...
+    if request.method == 'POST':  # If the form has been submitted...
         # ContactForm was defined in the previous section
-        form = PlayerForm(request.POST) # A form bound to the POST data
-        if form.is_valid(): # All validation rules pass
+        form = PlayerForm(request.POST)  # A form bound to the POST data
+        if form.is_valid():  # All validation rules pass
             # Process the data in form.cleaned_data
             # Great, a legit game!  Let's save it
-            
+
             game = Game()
             game.played_date = form.cleaned_data['game_date']
             game.entered_date = datetime.now()
             game.save()
-            
+
             summaries = []
             scores = []
-            
-            for i in range (1,7):
+
+            for i in range(1, 7):
                 player = form.cleaned_data['name' + str(i)]
                 if player:
                     # This player was entered
                     summary = PlayerGameSummary()
-                    
+
                     summary.player = player
                     summary.game = game
                     summary.score = form.cleaned_data['score' + str(i)]
                     summary.made_bid = form.cleaned_data['star' + str(i)]
-                    
+
                     summary.save()
-                    
+
                     summaries.append(summary)
                     scores.append(summary.score)
-                    
+
                 else:
                     # Left blank, and that's okay.
                     pass
-            
+
             utils.sortAndRankSummaries(summaries)
 
             for s in summaries:
                 s.save()
-            
-            return HttpResponseRedirect('/') # Redirect after POST
+
+            return HttpResponseRedirect('/')  # Redirect after POST
     else:
         form = PlayerForm()
 
@@ -95,7 +99,8 @@ def entry(request):
         'form': form,
     })
 
-'''    
+
+'''
 def entry(request):
     player_list = Player.objects.order_by('-first_name')[:5]
     template = loader.get_template('rookscore/entry.html')
@@ -105,12 +110,12 @@ def entry(request):
     return HttpResponse(template.render(context))    
 '''
 
+
 def games_repair(request):
     # Ensure everyone has the right rank
     for g in Game.objects.all():
         summaries = list(g.scores.all())
         utils.sortAndRankSummaries(summaries)
-
 
         # Temp - set opponents to be everyone who's not the caller or partner
         players = []
@@ -124,23 +129,24 @@ def games_repair(request):
         for bid in g.bids.all():
             # Temp
             opponents = []
-            
+
             for p in players:
                 if p != bid.caller and p not in bid.partners.all():
                     opponents.append(p)
-                    
+
             bid.opponents = opponents
-            
+
             bid.save()
-            
+
             # End Temp
-            
+
     # Update all of the Elo scores
     seasons = CacheManager().seasons().all()
     for s in seasons:
         Player.objects.rankings(s)
-    
+
     return HttpResponse("<html><body>Repair complete.</body></html>")
+
 
 def charts(request):
     games_list = Game.objects.order_by('-entered_date')
@@ -148,8 +154,9 @@ def charts(request):
     context = RequestContext(request, {
         'games_list': games_list,
     })
-    return HttpResponse(template.render(context))   
-    
+    return HttpResponse(template.render(context))
+
+
 def games(request):
     games_list = Game.objects.order_by('-entered_date')
     template = loader.get_template('rookscore/games.html')
@@ -158,17 +165,18 @@ def games(request):
     })
     return HttpResponse(template.render(context))
 
+
 def game(request, game_id):
-    
-#    from django.http import Http404
-#    def detail(request, poll_id):
-#    try:
-#        poll = Poll.objects.get(pk=poll_id)
-#    except Poll.DoesNotExist:
-#        raise Http404
-#    return render(request, 'polls/detail.html', {'poll': poll})
-    
+    #    from django.http import Http404
+    #    def detail(request, poll_id):
+    #    try:
+    #        poll = Poll.objects.get(pk=poll_id)
+    #    except Poll.DoesNotExist:
+    #        raise Http404
+    #    return render(request, 'polls/detail.html', {'poll': poll})
+
     return HttpResponse("You have requested game #" + str(game_id))
+
 
 def seasons(request):
     seasons = CacheManager().seasons().all()
@@ -177,15 +185,17 @@ def seasons(request):
         'seasons': seasons,
     })
     return HttpResponse(template.render(context))
-    
+
+
 def season(request, season_start):
     season = None
     for s in CacheManager().seasons().all():
         if CacheManager().seasons().get_key(s.start_date) == season_start:
             season = s
             break
-        
+
     return render_season(request, season)
+
 
 def players(request):
     players_list = Player.objects.order_by('first_name')
@@ -195,17 +205,18 @@ def players(request):
     })
     return HttpResponse(template.render(context))
 
+
 def player(request, player_id):
     try:
         player = Player.objects.get(pk=player_id)
     except Player.DoesNotExist:
         raise Http404
-        
-    all_players = Player.objects.all()        
+
+    all_players = Player.objects.all()
     recent_games = Game.objects.filter()
-    
+
     finishes = []
-    finishes.append(['4 Player', 0, 0, 0, 0, '', '', ])    
+    finishes.append(['4 Player', 0, 0, 0, 0, '', '', ])
     finishes.append(['5 Player', 0, 0, 0, 0, 0, '', ])
     finishes.append(['6 Player', 0, 0, 0, 0, 0, 0, ])
     finishes.append(['Total', 0, 0, 0, 0, 0, 0, ])
@@ -213,42 +224,43 @@ def player(request, player_id):
     for score in PlayerGameSummary.objects.filter(player=player):
         num_players = score.game.scores.count()
         if score.rank < 7:
-            finishes[num_players-4][score.rank] = finishes[num_players-4][score.rank] + 1
+            finishes[num_players - 4][score.rank] = finishes[num_players - 4][score.rank] + 1
             finishes[3][score.rank] = finishes[3][score.rank] + 1
-    
+
     all_awards = CacheManager().awards().all()
     player_awards = []
-    
+
     for award in all_awards:
         for season, winners in award.season_winners.items():
             # if season == SeasonCache().get(datetime.today()) and award.full_season_required:
             #     # Exclude seasonal awards that are in progress
             #     continue
-            
+
             for winner in winners:
                 if player in winner.players:
                     player_awards.append({
                         'award': award,
                         'season': season,
                         'winner': winner,
-                        })
+                    })
 
-    player_awards.sort(key=lambda x : x['season'].sort_key if x['season'] else 0, reverse=True)
+    player_awards.sort(key=lambda x: x['season'].sort_key if x['season'] else 0, reverse=True)
     current_season = SeasonCache().get(datetime.today())
 
-    return render(request, 'rookscore/player.html', 
-        {
-            'player': player,
-            'all_players': all_players,
-            'recent_games': recent_games,
-            'finishes': finishes,
-            'player_awards': player_awards,
-            'current_season': current_season
-        })
+    return render(request, 'rookscore/player.html',
+                  {
+                      'player': player,
+                      'all_players': all_players,
+                      'recent_games': recent_games,
+                      'finishes': finishes,
+                      'player_awards': player_awards,
+                      'current_season': current_season
+                  })
+
 
 def awards(request):
     start = datetime.now()
-    
+
     awards = CacheManager().awards().all()
     current_season = SeasonCache().get(datetime.today())
     template = loader.get_template('rookscore/awards.html')
@@ -256,10 +268,10 @@ def awards(request):
         'awards': awards,
         'current_season': current_season
     })
-    
+
     end = datetime.now()
-    print (str(end - start) + ' seconds to generate awards')
-    
+    print(str(end - start) + ' seconds to generate awards')
+
     return HttpResponse(template.render(context))
 
 
@@ -270,53 +282,60 @@ def awards(request):
 # GAMES
 
 class GameDetail(generics.RetrieveAPIView):
-    #permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
-    
+    # permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
     queryset = Game.objects.all()
     serializer_class = GameSerializer
-    
+
+
 class GameList(generics.ListCreateAPIView):
-    #permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
-    
+    # permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
     queryset = Game.objects.all()
     serializer_class = GameSerializer
+
 
 # PLAYERS
 
 class PlayerDetail(generics.RetrieveAPIView):
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
-    
+
     queryset = Player.objects.all()
     serializer_class = PlayerSerializer
-    
+
+
 class PlayerList(generics.ListCreateAPIView):
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
-    
+
     queryset = Player.objects.all()
     serializer_class = PlayerSerializer
+
 
 # SCORES
 class ScoreDetail(generics.RetrieveAPIView):
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
-    
+
     queryset = PlayerGameSummary.objects.all()
     serializer_class = ScoreSerializer
-    
+
+
 class ScoreList(generics.ListCreateAPIView):
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
-    
+
     queryset = PlayerGameSummary.objects.all()
     serializer_class = ScoreSerializer
-    
+
+
 # BIDS
 class BidDetail(generics.RetrieveAPIView):
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
-    
+
     queryset = Bid.objects.all()
     serializer_class = BidSerializer
-    
+
+
 class BidList(generics.ListCreateAPIView):
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
-    
+
     queryset = Bid.objects.all()
     serializer_class = BidSerializer
